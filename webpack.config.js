10c538e5f05fa87041f5168423b259699764193c
Pranesh Ravi <praneshpranesh@gmail.com>
2019-08-03 23:03:23 +0530

    Add prod config

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ const path = require('path');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {
+  CleanWebpackPlugin,
+} = require('clean-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: {
     index: path.join(__dirname, 'src', 'ui', 'index.ts'),
     plugin: path.join(__dirname, 'src', 'plugin.ts'),
   },
-  devtool: 'inline-source-map',
-  mode: 'development',
+  mode: isProd ? 'production' : 'development',
   output: {
     path: path.join(__dirname, 'plugin'),
     publicPath: '/',
@@ -29,6 +33,7 @@ module.exports = {
       inject: 'head',
       inlineSource: '.(js)$',
       chunks: ['index'],
+      minify: true,
       template: path.join(__dirname, 'index.html'),
     }),
     new HtmlWebpackInlineSourcePlugin(),
@@ -36,5 +41,10 @@ module.exports = {
       from: path.join(__dirname, 'manifest.json'),
       to: path.join(__dirname, 'plugin', 'manifest.json'),
     }]),
-  ],
+  ].concat(
+    isProd ? new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: path.join(__dirname, 'plugin'),
+      cleanAfterEveryBuildPatterns: path.join(__dirname, 'plugin', 'index.js'),
+    }) : [],
+  ),
 };
